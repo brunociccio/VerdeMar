b244aab3446f09d12070f48bd3d683555e711ec6
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-05-29 13:49:54 -0300

    upload dos primeiros arquivos do projeto, com todas as dependencias que serão utilizadas e com algumas configurações no arquivo application properties

---
+++ src/main/java/br/com/verdemar/VerdemarApplication.java
@@ -0,0 +1,34 @@
+package br.com.verdemar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@SpringBootApplication
+@Controller
+@OpenAPIDefinition(
+		info = @Info(
+		title = "VerdeMar API",
+		version = "1.0.0",
+		description = "API do app VerdeMar",
+		contact = @Contact(name = "Bruno Ciccio", email = "dev.bruno.ciccio@gmail.com")
+	)
+)
+public class VerdemarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerdemarApplication.class, args);
+	}
+
+	@RequestMapping("/home")
+    @ResponseBody
+    public String home() {
+        return "WebApp VerdeMar";
+    }
+}

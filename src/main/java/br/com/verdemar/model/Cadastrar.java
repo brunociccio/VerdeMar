2302b60fa6c4a632624fc9002d7a205819b1bbfb
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-06-05 10:19:21 -0300

    criação das classes: cadastrarcnpj, contato, documentos, endereco, pontocoleta e usuario

--- src/main/java/br/com/verdemar/model/Cadastrar.java
+++ src/main/java/br/com/verdemar/model/Cadastrar.java
@@ -1,5 +1,47 @@
 package br.com.verdemar.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class Cadastrar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Nome de usuário é obrigatório")
+    private String nomeUsuario;
+
+    @NotBlank(message = "Senha é obrigatória")
+    private String senha;
+
+    @Email(message = "Email inválido")
+    @NotBlank(message = "Email é obrigatório")
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Documentos documentos;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Endereco endereco;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Contato contato;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private CadastrarCnpj cadastrarCnpj;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private PontoColeta pontoColeta;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Usuario usuario;
 
 }
---
+++ src/main/java/br/com/verdemar/model/Documentos.java
@@ -0,0 +1,5 @@
+package br.com.verdemar.model;
+
+public class Documentos {
+
+}

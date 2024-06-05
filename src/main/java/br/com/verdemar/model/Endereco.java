b68b8a7b23102fc9b5eab24f16c24965a63ef52e
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-06-05 18:51:14 -0300

    update da classe model endereço para sincronizar os testes das saidas dos status code no insomnia

--- src/main/java/br/com/verdemar/model/Endereco.java
+++ src/main/java/br/com/verdemar/model/Endereco.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -27,8 +29,9 @@ public class Endereco {
     @NotBlank(message = "Logradouro é obrigatório")
     private String logradouro;
 
-    @NotBlank(message = "Número é obrigatório")
-    private int numero;
+    @NotNull(message = "Número é obrigatório")
+    @Min(value = 1, message = "Número deve ser maior que zero")
+    private Integer numero;
 
     @NotBlank(message = "Complemento é obrigatório")
     private String complemento;
